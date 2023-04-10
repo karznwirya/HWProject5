a674008702f23b19a678b55555053e7419479a09
karza <karza@10.0.0.52>
2023-04-10 11:29:49 -0400

    IMplement getInfo()

--- src/Demo5/TEST5.java
+++ src/Demo5/TEST5.java
@@ -5,4 +5,8 @@ public class TEST5 {
 	public static void main(String[] args) {
 		System.out.println("TEST%5");
 	}
+	
+	public void getinfo1() {
+		System.out.println("pushing seconf time,,,");
+	}
 }
